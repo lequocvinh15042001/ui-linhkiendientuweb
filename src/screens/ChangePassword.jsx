b21854e1ac6b164c6633902440e419ef9c7e8b30
Duong2k1 <19110009@student.hcmute.edu.vn>
2022-11-30 13:44:57 +0700

    Update verify

--- src/screens/ChangePassword.jsx
+++ src/screens/ChangePassword.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form, OverlayTrigger, Popover } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
@@ -43,32 +43,48 @@ const ChangePassword = () => {
         }
     }
 
+    // Check level password
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Mức độ mật khẩu</Popover.Header>
+            <Popover.Body>
+                <p><i style={{ color: (newPassword.length < 8 || newPassword.length > 20 ? 'red' : 'green'), fontSize: "20px" }} class="fa fa-check-circle" aria-hidden="true"></i> Lớn hơn 8 và nhỏ hơn 20 ký tự</p>
+                <p><i style={{ color: (!newPassword.match(/[A-Z]/) ? 'red' : 'green'), fontSize: "20px" }} class="fa fa-check-circle" aria-hidden="true"></i> Ít nhất 1 ký tự viết HOA</p>
+                <p><i style={{ color: (!newPassword.match(/[a-z]/) ? 'red' : 'green'), fontSize: "20px" }} class="fa fa-check-circle" aria-hidden="true"></i> Ít nhất 1 ký tự viết THƯỜNG</p>
+                <p><i style={{ color: (!newPassword.match(/[\`~!@#$%\^&*()+=|;:'",.<>\/?\\\-]/) ? 'red' : 'green'), fontSize: "20px" }} class="fa fa-check-circle" aria-hidden="true"></i> Ít nhất 1 ký tự đặc biệt</p>
+                <p><i style={{ color: (!newPassword.match(/[\d]/) ? 'red' : 'green'), fontSize: "20px" }} class="fa fa-check-circle" aria-hidden="true"></i> Ít nhất 1 ký tự số</p>
+            </Popover.Body>
+        </Popover >
+    );
+
     return (
-        <div className='container' style={{"paddingTop":"10rem"}}>
-        <FormContainer>
-            <h1 className='d-flex justify-content-center py-3'>Thay đổi mật khẩu</h1>
-            {message && <Message variant='danger'>{message}</Message>}
-            {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
-            <Form onSubmit={submitHandler} >
-                {/* <Form.Group controlId='email'>
+        <div className='container' style={{ "paddingTop": "10rem" }}>
+            <FormContainer>
+                <h1 className='d-flex justify-content-center py-3'>Thay đổi mật khẩu</h1>
+                {message && <Message variant='danger'>{message}</Message>}
+                {error && <Message variant='danger'>{error}</Message>}
+                {loading && <Loader />}
+                <Form onSubmit={submitHandler} >
+                    {/* <Form.Group controlId='email'>
                     <Form.Label>Email</Form.Label>
                     <Form.Control type='email' placeholder='Nhập email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
                 </Form.Group> */}
-                <Form.Group controlId='password' className='py-3'>
-                    <Form.Label>Mật khẩu cũ</Form.Label>
-                    <Form.Control type='password' placeholder='Nhập mật khẩu' value={oldPassword} onChange={(e) => setOldPassword(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group controlId='password'>
-                    <Form.Label>Mật khẩu mới</Form.Label>
-                    <Form.Control type='password' placeholder='Nhập mật khẩu' value={newPassword} onChange={(e) => setNewPassword(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group className='d-flex justify-content-between py-3'>
-                    <Link className='text-danger' to='/'>Hủy</Link>
-                    <Button type='submit' variant='primary'>Đổi mật khẩu</Button>
-                </Form.Group>
-            </Form>
-        </FormContainer>
+                    <Form.Group controlId='password' className='py-3'>
+                        <Form.Label>Mật khẩu cũ</Form.Label>
+                        <Form.Control type='password' placeholder='Nhập mật khẩu' value={oldPassword} onChange={(e) => setOldPassword(e.target.value)}></Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Mật khẩu mới</Form.Label>
+                        <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                            <Form.Control type='password' placeholder='Nhập mật khẩu' value={newPassword} onChange={(e) => setNewPassword(e.target.value)}></Form.Control>
+                        </OverlayTrigger>
+                    </Form.Group>
+                    <Form.Group className='d-flex justify-content-between py-3'>
+                        <Link className='text-danger' to='/'>Hủy</Link>
+                        <Button style={{ width: 'auto' }} disabled={((newPassword.length < 8 || newPassword.length > 20) || !newPassword.match(/[A-Z]/) || !newPassword.match(/[a-z]/) || !newPassword.match(/[\`~!@#$%\^&*()+=|;:'",.<>\/?\\\-]/) || !newPassword.match(/[\d]/)) ? 'true' : ''} type='submit' variant='primary'>Đổi mật khẩu</Button>
+                    </Form.Group>
+                </Form>
+            </FormContainer>
         </div>
     )
 }
